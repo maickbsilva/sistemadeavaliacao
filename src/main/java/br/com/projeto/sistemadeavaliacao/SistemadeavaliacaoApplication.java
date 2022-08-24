4701761997f388427bde8663117941d0a6731b65
Maick <maickbsilva@gmail.com>
2022-08-24 19:11:59 -0300

    first commit

---
+++ src/main/java/br/com/projeto/sistemadeavaliacao/SistemadeavaliacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.sistemadeavaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadeavaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadeavaliacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/sistemadeavaliacao/SistemadeavaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.sistemadeavaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadeavaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
