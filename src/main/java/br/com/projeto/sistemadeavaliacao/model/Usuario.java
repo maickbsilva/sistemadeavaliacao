4d5584259d24513658609c996fe37d02499b96e7
Luiz Ortiz <luizortiz2010@hotmail.com>
2022-08-26 18:51:07 -0300

    Ticket - 1

---
+++ src/main/java/br/com/projeto/sistemadeavaliacao/model/Usuario.java
@@ -0,0 +1,51 @@
+package br.com.projeto.sistemadeavaliacao.model;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * Classe responsável pelo gerenciamento da entidade Usuario.
+ * 
+ * @author Projeto Sistema de Avaliação
+ *
+ */
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userID;
+	private String senha;
+	@Column(nullable = false, unique = true)
+	@NotEmpty
+	private String nif;
+	public Long getUserID() {
+		return userID;
+	}
+	public void setUserID(Long userID) {
+		this.userID = userID;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getNif() {
+		return nif;
+	}
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+	
+		
+	}
