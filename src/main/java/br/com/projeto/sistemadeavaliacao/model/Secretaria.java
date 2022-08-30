b05ef53b124689694be083327ac54e847e1e29a6
Maick <maickbsilva@gmail.com>
2022-08-30 19:15:13 -0300

    correcoes

--- src/main/java/br/com/projeto/sistemadeavaliacao/model/Secretaria.java
+++ src/main/java/br/com/projeto/sistemadeavaliacao/model/Secretaria.java
@@ -2,21 +2,9 @@ package br.com.projeto.sistemadeavaliacao.model;
 
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
-import javax.validation.constraints.NotEmpty;
 
 @Entity
 @PrimaryKeyJoinColumn(name = "userID")
 public class Secretaria extends Usuario {
-
-	@NotEmpty
-	private String nome;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 
 }
