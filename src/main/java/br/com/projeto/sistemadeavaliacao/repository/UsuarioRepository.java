570e85f7ee420dfb6123bcf04580a3265382c3d7
Maick <maickbsilva@gmail.com>
2022-10-04 19:59:45 -0300

    sprint-2-docentes-por-pesquisa

--- src/main/java/br/com/projeto/sistemadeavaliacao/repository/UsuarioRepository.java
+++ src/main/java/br/com/projeto/sistemadeavaliacao/repository/UsuarioRepository.java
@@ -1,5 +1,8 @@
 package br.com.projeto.sistemadeavaliacao.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -20,6 +23,9 @@ public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, L
 	
 	//@Query("SELECT c FROM Usuario c WHERE c.nome LIKE %:t% OR c.nif LIKE %:t% OR c.tipo LIKE %:t% ")
 	//public List<Usuario> Buscar(@Param("t") String tudo);
+
+	@Query("SELECT u FROM Usuario u WHERE u.tipo = 1")
+	public List<Usuario> BuscarDocentes();
 	
 
 }
