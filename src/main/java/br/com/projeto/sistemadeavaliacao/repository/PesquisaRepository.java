947f6a317cdca8b3a92ec7a6ca75d5ef7bd1fb23
Maick <maickbsilva@gmail.com>
2022-10-04 22:25:05 -0300

    remove pergunta in progress

--- src/main/java/br/com/projeto/sistemadeavaliacao/repository/PesquisaRepository.java
+++ src/main/java/br/com/projeto/sistemadeavaliacao/repository/PesquisaRepository.java
@@ -6,4 +6,7 @@ import br.com.projeto.sistemadeavaliacao.model.Pesquisa;
 
 public interface PesquisaRepository extends CrudRepository<Pesquisa, Long>{
 
+    /*@Query("SELECT p FROM Pergunta p LEFT JOIN p.listaPesquisa pe where size(p.listaPesquisa) = 0 OR pe.id = :id")
+    public List<Pergunta> filtroPerguntas(@Param("id") Long idPesquisa); */
+    
 }
