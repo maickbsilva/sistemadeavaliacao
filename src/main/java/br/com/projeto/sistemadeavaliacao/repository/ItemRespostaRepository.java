9d8dd03109a328b44ba3d3b293481d1fb06901b9
Maick <maickbsilva@gmail.com>
2022-11-03 22:26:15 -0300

    in progress

--- src/main/java/br/com/projeto/sistemadeavaliacao/repository/ItemRespostaRepository.java
+++ src/main/java/br/com/projeto/sistemadeavaliacao/repository/ItemRespostaRepository.java
@@ -1,10 +1,15 @@
 package br.com.projeto.sistemadeavaliacao.repository;
 
+import br.com.projeto.sistemadeavaliacao.model.Resposta;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.projeto.sistemadeavaliacao.model.ItemResposta;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRespostaRepository extends PagingAndSortingRepository<ItemResposta, Long>{
-    
+
+    @Query("SELECT ir FROM ItemResposta ir where ir.resposta = :id")
+    public Resposta satisfacoesPorResposta(@Param("id") Long idResposta);
     
 }
--- src/main/java/br/com/projeto/sistemadeavaliacao/repository/RespostaRepository.java
+++ src/main/java/br/com/projeto/sistemadeavaliacao/repository/RespostaRepository.java
@@ -1,9 +1,12 @@
 package br.com.projeto.sistemadeavaliacao.repository;
 
+import br.com.projeto.sistemadeavaliacao.model.Pesquisa;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.projeto.sistemadeavaliacao.model.Resposta;
+import org.springframework.data.repository.query.Param;
 
 public interface RespostaRepository extends PagingAndSortingRepository<Resposta, Long>{
-    
+
 }
