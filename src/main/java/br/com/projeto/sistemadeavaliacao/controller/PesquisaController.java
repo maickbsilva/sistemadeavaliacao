e552921baf86c6b27ecb7c896978b7f59057f5a0
ArthurCorpO <arthur.corporativo@outlook.com>
2022-11-08 18:26:37 -0300

    "reconstruindo lista de respostas"

--- src/main/java/br/com/projeto/sistemadeavaliacao/controller/PesquisaController.java
+++ src/main/java/br/com/projeto/sistemadeavaliacao/controller/PesquisaController.java
@@ -3,13 +3,17 @@ package br.com.projeto.sistemadeavaliacao.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.projeto.sistemadeavaliacao.annotation.DiretorAnnotation;
 import br.com.projeto.sistemadeavaliacao.annotation.SecretariaAnnotation;
 import br.com.projeto.sistemadeavaliacao.model.Pesquisa;
+import br.com.projeto.sistemadeavaliacao.model.Resposta;
 import br.com.projeto.sistemadeavaliacao.repository.CursoRepository;
 import br.com.projeto.sistemadeavaliacao.repository.ItemRespostaRepository;
 import br.com.projeto.sistemadeavaliacao.repository.PerguntaRepository;
@@ -84,6 +88,16 @@ public class PesquisaController {
 	public String listaPesquisa(Model model) {
 		model.addAttribute("pesq", pesquisaRepository.findAll());
 		return "pesquisa/listaPesquisa";
+	}
+
+	@PostMapping("*/pesquisarid")
+	public ModelAndView pesquisar(@RequestParam("idpesquisa") Long idpesquisa){
+		ModelAndView modelAndView = new ModelAndView("pesquisa/listaPesquisa");
+		modelAndView.addObject("listaResposta", respostaRepository.findById(idpesquisa));
+		modelAndView.addObject("listaItem", itemRepository.findById(idpesquisa));
+		modelAndView.addObject("listaResposta", new Pesquisa());
+		return modelAndView;
+
 	}
 
 }
---
+++ src/main/resources/static/js/focus.js
@@ -0,0 +1,9 @@
+document.addEventListener("keydown", function (e) {
+
+    if (e.keyCode === 13) {
+
+        e.preventDefault();
+
+    }
+
+});
