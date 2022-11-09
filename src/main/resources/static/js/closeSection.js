028370a9a99f920dcbf907ca04dd4bc8b34c4c7b
ArthurCorpO <arthur.corporativo@outlook.com>
2022-11-09 20:51:26 -0300

    "exibicao das respostas"

--- src/main/resources/static/js/closeSection.js
+++
@@ -1,9 +0,0 @@
-$('#exb').on('click', function(){
-    $('menu').slideToggle('slow');
-});
-
-$('.slc').each(function(){
-    $(this).on('click', function(){
-        $('slc').slideToggle('slow');
-    });
-});
