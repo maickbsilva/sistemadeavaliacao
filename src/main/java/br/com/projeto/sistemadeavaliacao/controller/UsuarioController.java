0beb7ddb52e62a87ffc279dd7b4e9eeaa9c1e5d4
Maick <maickbsilva@gmail.com>
2022-11-11 01:25:52 -0300

    desativar usuario ok + correcoes listas e cads

--- src/main/java/br/com/projeto/sistemadeavaliacao/controller/UsuarioController.java
+++ src/main/java/br/com/projeto/sistemadeavaliacao/controller/UsuarioController.java
@@ -34,202 +34,195 @@ import br.com.projeto.sistemadeavaliacao.util.HashUtil;
 @Controller
 public class UsuarioController {
 
-	@Autowired
-	private UsuarioRepository repository;
+    @Autowired
+    private UsuarioRepository repository;
 
-	@Autowired
-	private PesquisaRepository pesquisaRepository;
+    @Autowired
+    private PesquisaRepository pesquisaRepository;
 
-	@SecretariaAnnotation
-	@DiretorAnnotation
-	@RequestMapping("cadastro")
-	private String formularioCadastro(Model model) {
-		return "cadastro/cadastroUsuario";
-	}
+    @SecretariaAnnotation
+    @DiretorAnnotation
+    @RequestMapping("cadastro")
+    private String formularioCadastro(Model model) {
+        return "cadastro/cadastroUsuario";
+    }
 
-	@SecretariaAnnotation
-	@DiretorAnnotation
-	@RequestMapping(value = "save", method = RequestMethod.POST)
-	public String salvarUsuario(@Valid Usuario usuario, BindingResult result, RedirectAttributes attr,
-			TipoUsuario tipo) {
+    @SecretariaAnnotation
+    @DiretorAnnotation
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String salvarUsuario(@Valid Usuario usuario, BindingResult result, RedirectAttributes attr,
+                                TipoUsuario tipo) {
 
-		if (result.hasErrors()) {
-			attr.addFlashAttribute("mensagemErro", "verificar os campos novamente...");
-			return "redirect:cadastro";
+        if (result.hasErrors()) {
+            attr.addFlashAttribute("mensagemErro", "verificar os campos novamente...");
+            return "redirect:cadastro";
 
-		}
+        }
 
-		if (usuario.getSenha().equals(HashUtil.hash(""))) {
+        if (usuario.getSenha().equals(HashUtil.hash(""))) {
 
-			String hash = repository.findById(usuario.getUserId()).get().getSenha();
-			usuario.setSenha(hash);
-		}
+            String hash = repository.findById(usuario.getUserId()).get().getSenha();
+            usuario.setSenha(hash);
+        }
 
-		try
+        try {
+            repository.save(usuario);
+            attr.addFlashAttribute("mensagemSucesso", "Administrador cadastrado com sucesso ID:" + usuario.getUserId());
+        } catch (Exception e) {
+            System.out.println("erro ao cadastrar");
+            attr.addFlashAttribute("mensagemErro", "Verificar os campos novamente, Este Nif já existe");
+        }
+        return "redirect:cadastro";
+    }
 
-		{
-			repository.save(usuario);
-			attr.addFlashAttribute("mensagemSucesso", "Administrador cadastrado com sucesso ID:" + usuario.getUserId());
-		} catch (Exception e) {
-			System.out.println("erro ao cadastrar");
-			attr.addFlashAttribute("mensagemErro", "Verificar os campos novamente, Este Nif já existe");
-		}
-		return "redirect:cadastro";
-	}
+    @SecretariaAnnotation
+    @DiretorAnnotation
+    @RequestMapping("lista/{page}")
+    public String lista(Model model, @PathVariable("page") int page) {
 
-	@SecretariaAnnotation
-	@DiretorAnnotation
-	@RequestMapping("lista/{page}")
-	public String lista(Model model, @PathVariable("page") int page) {
+        PageRequest pageble = PageRequest.of(page - 1, 6, Sort.by(Sort.Direction.ASC, "nome"));
 
-		PageRequest pageble = PageRequest.of(page - 1, 6, Sort.by(Sort.Direction.ASC, "nome"));
+        Page<Usuario> pagina = repository.findAll(pageble);
 
-		Page<Usuario> pagina = repository.findAll(pageble);
+        model.addAttribute("usuario", pagina.getContent());
 
-		model.addAttribute("usuario", pagina.getContent());
+        int totalPaginas = pagina.getTotalPages();
 
-		int totalPaginas = pagina.getTotalPages();
+        List<Integer> numPaginas = new ArrayList<Integer>();
 
-		List<Integer> numPaginas = new ArrayList<Integer>();
+        for (int i = 1; i <= totalPaginas; i++) {
+            numPaginas.add(i);
+        }
 
-		for (int i = 1; i <= totalPaginas; i++) {
-			numPaginas.add(i);
-		}
+        model.addAttribute("numPagina", numPaginas);
+        model.addAttribute("totalPages", totalPaginas);
+        model.addAttribute("pagAtual", page);
 
-		model.addAttribute("numPagina", numPaginas);
-		model.addAttribute("totalPages", totalPaginas);
-		model.addAttribute("pagAtual", page);
+        return "listas/listaUsuarios";
 
-		return "listas/listaUsuarios";
+    }
 
-	}
+    @SecretariaAnnotation
+    @DiretorAnnotation
+    @RequestMapping("alterar")
+    public String alterar(Long userId, Model model) {
+        Usuario usuario = repository.findById(userId).get();
+        model.addAttribute("users", usuario);
+        return "forward:cadastro";
+    }
 
-	@SecretariaAnnotation
-	@DiretorAnnotation
-	@RequestMapping("alterar")
-	public String alterar(Long userId, Model model) {
-		Usuario usuario = repository.findById(userId).get();
-		model.addAttribute("users", usuario);
-		return "forward:cadastro";
-	}
+    @SecretariaAnnotation
+    @DiretorAnnotation
+    @RequestMapping("desativar")
+    public String desativar(Long userId) {
+        Usuario u = repository.findById(userId).get();
+        u.setAtivo(false);
+        repository.save(u);
+        return "redirect:lista/1";
+    }
 
-	@SecretariaAnnotation
-	@DiretorAnnotation
-	@RequestMapping("excluir")
-	public String excluir(Long userId) {
-		Usuario excluir = repository.findById(userId).get();
-		repository.delete(excluir);
-		return "redirect:lista/1";
-	}
+    @DiretorAnnotation
+    @RequestMapping("telaInicialDiretor")
+    public String telaInicialDiretor() {
+        return "telainicial/telaInicialDiretor";
+    }
 
-	@DiretorAnnotation
-	@RequestMapping("telaInicialDiretor")
-	public String telaInicialDiretor() {
-		return "telainicial/telaInicialDiretor";
-	}
+    @DocenteAnnotation
+    @RequestMapping("telaInicialDocencia")
+    public String telaInicialDocencia(Model model, HttpServletRequest request) {
+        Usuario u = (Usuario) request.getSession().getAttribute("usuarioLogado");
+        Long id = u.getUserId();
+        model.addAttribute("pesq", pesquisaRepository.listaPesquisaPorDocente(id));
+        return "telainicial/telaInicialDocencia";
+    }
 
-	@DocenteAnnotation
-	@RequestMapping("telaInicialDocencia")
-	public String telaInicialDocencia(Model model, HttpServletRequest request) {
-		Usuario u = (Usuario) request.getSession().getAttribute("usuarioLogado");
-		Long id = u.getUserId();
-		model.addAttribute("pesq", pesquisaRepository.listaPesquisaPorDocente(id));
-		return "telainicial/telaInicialDocencia";
-	}
+    @SecretariaAnnotation
+    @RequestMapping("telaInicialSecretaria")
+    public String telaInicialSecretaria() {
+        return "telainicial/telaInicialSecretaria";
+    }
 
-	@SecretariaAnnotation
-	@RequestMapping("telaInicialSecretaria")
-	public String telaInicialSecretaria() {
-		return "telainicial/telaInicialSecretaria";
-	}
+    @PublicoAnnotation
+    @RequestMapping("login")
+    public String login(Usuario admLogin, RedirectAttributes attr, HttpSession session, Model model) {
 
-	@PublicoAnnotation
-	@RequestMapping("login")
-	public String login(Usuario admLogin, RedirectAttributes attr, HttpSession session, Model model) {
+        Usuario user = repository.findByNifAndSenhaAndAtivo(admLogin.getNif(), admLogin.getSenha(), true);
 
-		Usuario user = repository.findByNifAndSenha(admLogin.getNif(), admLogin.getSenha());
+        if (user == null) {
+            attr.addFlashAttribute("mensagemErro", "verificar os campos novamente...");
+            return "redirect:/";
+        } else {
+            //trocando a senha caso usuario esteja com a senha padrao
+            if (user.getSenha().equals(HashUtil.hash("sistema"))) {
+                model.addAttribute("usuario", user);
+                return "util/Model";
+            }
 
-		if (user == null) {
-			
+            if (user.getTipo() != null && user.getTipo() == TipoUsuario.DIRETOR) {
+                session.setAttribute("usuarioLogado", user);
+                session.setAttribute("nivel", user.getTipo());
+                return "redirect:/telaInicialDiretor";
 
-			/*!Mensagem de erro de Usuario NULO
-			!
-			!
-			*/
-			attr.addFlashAttribute("mensagemErro", "verificar os campos novamente...");	
-			return "redirect:/";	
-		} else {
-			//trocando a senha caso usuario esteja com a senha padrao
-			if (user.getSenha().equals(HashUtil.hash("sistema"))) {
-				model.addAttribute("usuario", user);
-				return "util/Model";
+            } else if (user.getTipo() != null && user.getTipo() == TipoUsuario.SECRETARIA) {
+                session.setAttribute("usuarioLogado", user);
+                session.setAttribute("nivel", user.getTipo());
+                return "redirect:/telaInicialSecretaria";
 
-			}
+            } else if (user.getTipo() != null && user.getTipo() == TipoUsuario.DOCENCIA) {
+                session.setAttribute("usuarioLogado", user);
+                session.setAttribute("nivel", user.getTipo());
+                return "redirect:/telaInicialDocencia";
+            }
+        }
+        return "login/login";
 
-			if (user.getTipo() != null && user.getTipo() == TipoUsuario.DIRETOR) {
-				session.setAttribute("usuarioLogado", user);
-				session.setAttribute("nivel", user.getTipo());
-				return "redirect:/telaInicialDiretor";
+    }
 
-			} else if (user.getTipo() != null && user.getTipo() == TipoUsuario.SECRETARIA) {
-				session.setAttribute("usuarioLogado", user);
-				session.setAttribute("nivel", user.getTipo());
-				return "redirect:/telaInicialSecretaria";
-			}else if (user.getTipo() != null && user.getTipo() == TipoUsuario.DOCENCIA){
-				session.setAttribute("usuarioLogado", user);
-				session.setAttribute("nivel", user.getTipo());
-				return "redirect:/telaInicialDocencia";
-			}
-		}
-		return "login/login";
 
-	}
+    @RequestMapping(value = "buscarUser", method = RequestMethod.GET)
+    public String buscar(String buscar, Model model) {
+        model.addAttribute("users", repository.Buscar(buscar));
+        return "listas/listaBuscaUsuario";
+    }
 
-	
-	@RequestMapping(value = "buscarUser", method = RequestMethod.GET)
-	public String buscar(String buscar, Model model){
-		model.addAttribute("users", repository.Buscar(buscar));
-		return "listas/listaBuscaUsuario";
-	}
+    @RequestMapping("logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "login/login";
 
-	@RequestMapping("logout")
-	public String logout(HttpSession session) {
-		session.invalidate();
-		return "login/login";
+    }
 
-	}
-	
-	@RequestMapping("/")
-	public String acesso() {
-		return "login/login";
-	}
-	
-	@SecretariaAnnotation
-	@RequestMapping("reseta")
-	public String reseta(Long userId, Model model) {
-		Usuario usuario = repository.findById(userId).get();
-		String sistema = "sistema";
-		usuario.setSenha(sistema);
-		repository.save(usuario);
-		System.out.println(usuario.getSenha() + "Senha alterada");
-		// alert senha redefinida...
+    @RequestMapping("/")
+    public String acesso() {
+        return "login/login";
+    }
 
-		// redirecionar mensagem senha de usuario alterada
-		return "redirect:lista/1";
-	}
-	
-	// --//--//
-	@RequestMapping(value = "alteraSenha", method = RequestMethod.POST)
-	private String formularioSenha(Long userId, String senha) {
-		System.out.println(userId + senha);
+    @SecretariaAnnotation
+    @RequestMapping("reseta")
+    public String reseta(Long userId, Model model) {
+        Usuario usuario = repository.findById(userId).get();
+        String sistema = "sistema";
+        usuario.setSenha(sistema);
+        repository.save(usuario);
+        System.out.println(usuario.getSenha() + "Senha alterada");
+        // alert senha redefinida...
 
-		Usuario oldUsuario = repository.findById(userId).get(); // Erro id Null
+        // redirecionar mensagem senha de usuario alterada
+        return "redirect:lista/1";
+    }
 
-		oldUsuario.setSenha(senha);
+    // --//--//
+    @RequestMapping(value = "alteraSenha", method = RequestMethod.POST)
+    private String formularioSenha(Long userId, String senha) {
+        System.out.println(userId + senha);
 
-		repository.save(oldUsuario);
-		return "login/login";
-	}
+        Usuario oldUsuario = repository.findById(userId).get(); // Erro id Null
+
+        oldUsuario.setSenha(senha);
+
+        repository.save(oldUsuario);
+        return "login/login";
+    }
 
 
 }
