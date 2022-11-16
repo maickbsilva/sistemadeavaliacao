d91eea5c41df7a505b94b269dadc12db789bec86
leandro <l34ndr0penha@gmail.com>
2022-11-16 19:09:46 -0300

    "pag modal progresso"

---
+++ src/main/resources/static/js/modal/modal.js
@@ -0,0 +1,12 @@
+document.getElementById('olho').addEventListener('mousedown', function() {
+    document.getElementById('pass').type = 'text';
+  });
+  
+  document.getElementById('olho').addEventListener('mouseup', function() {
+    document.getElementById('pass').type = 'password';
+  });
+  
+  // Para que o password não fique exposto apos mover a imagem.
+  document.getElementById('olho').addEventListener('mousemove', function() {
+    document.getElementById('pass').type = 'password';
+  });
