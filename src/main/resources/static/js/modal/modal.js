3eb1cbd6cd84117bdd17b1d6a7d618c307047e40
leandro <l34ndr0penha@gmail.com>
2022-11-16 19:19:26 -0300

    "visualizar senha ou não"

--- src/main/resources/static/js/modal/modal.js
+++ src/main/resources/static/js/modal/modal.js
@@ -6,7 +6,7 @@ document.getElementById('olho').addEventListener('mousedown', function() {
     document.getElementById('pass').type = 'password';
   });
   
-  // Para que o password não fique exposto apos mover a imagem.
+  //Para que o password não fique exposto apos mover a imagem.
   document.getElementById('olho').addEventListener('mousemove', function() {
     document.getElementById('pass').type = 'password';
   });
