f3adfcbb935ae701d25bcc91cd20bbd165cddd19
Maick <maickbsilva@gmail.com>
2022-11-17 22:05:26 -0300

    em progresso

--- src/main/java/br/com/projeto/sistemadeavaliacao/controller/ItemRespostaController.java
+++ src/main/java/br/com/projeto/sistemadeavaliacao/controller/ItemRespostaController.java
@@ -2,21 +2,12 @@ package br.com.projeto.sistemadeavaliacao.controller;
 
 import br.com.projeto.sistemadeavaliacao.model.*;
 import br.com.projeto.sistemadeavaliacao.repository.*;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import br.com.projeto.sistemadeavaliacao.annotation.DiretorAnnotation;
-import br.com.projeto.sistemadeavaliacao.annotation.DocenteAnnotation;
-import br.com.projeto.sistemadeavaliacao.annotation.SecretariaAnnotation;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.Date;
-import java.util.Map;
 
 @Controller
 @RequestMapping("itemResposta/")
